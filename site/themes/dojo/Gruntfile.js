9f0c1e49a13c30c44fe78b1df4880aca67e90378
Torrey Rice <rice@sitepen.com>
2017-02-10 22:10:53 -0800

    Getting stylus building properly on generate
    turns out you have to turn off sourcemaps or it throws an error.

--- site/themes/dojo/Gruntfile.js
+++ site/themes/dojo/Gruntfile.js
@@ -1,9 +1,37 @@
 module.exports = function(grunt){
   grunt.initConfig({
+    gitclone: {
+      fontawesome: {
+        options: {
+          repository: 'https://github.com/FortAwesome/Font-Awesome.git',
+          directory: 'tmp/fontawesome'
+        },
+      },
+      fancybox: {
+        options: {
+          repository: 'https://github.com/fancyapps/fancyBox.git',
+          directory: 'tmp/fancybox'
+        }
+      }
+    },
     copy: {
+      fontawesome: {
+        expand: true,
+        cwd: 'tmp/fontawesome/fonts/',
+        src: ['**'],
+        dest: 'source/css/fonts/'
+      },
+      fancybox: {
+        expand: true,
+        cwd: 'tmp/fancybox/source/',
+        src: ['**'],
+        dest: 'source/fancybox/'
+      }
     },
     _clean: {
-      tmp: ['tmp']
+      tmp: ['tmp'],
+      fontawesome: ['source/css/fonts'],
+      fancybox: ['source/fancybox']
     }
   });
 
@@ -11,6 +39,8 @@ module.exports = function(grunt){
 
   grunt.renameTask('clean', '_clean');
 
-  grunt.registerTask('default', ['copy', '_clean:tmp']);
+  grunt.registerTask('fontawesome', ['gitclone:fontawesome', 'copy:fontawesome', '_clean:tmp']);
+  grunt.registerTask('fancybox', ['gitclone:fancybox', 'copy:fancybox', '_clean:tmp']);
+  grunt.registerTask('default', ['gitclone', 'copy', '_clean:tmp']);
   grunt.registerTask('clean', ['_clean']);
 };
