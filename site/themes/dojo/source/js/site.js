fe29dbb6edcbd1a0a6113ab30112b4140593fa19
Torrey Rice <rice@sitepen.com>
2017-03-22 08:32:06 -0700

    Fixing issue with iframe in tutorial
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- site/themes/dojo/source/js/site.js
+++ site/themes/dojo/source/js/site.js
@@ -1,14 +1,16 @@
-var headroom = new Headroom(document.querySelector(".site-header"), {
-	"offset": 150,
-	tolerance: {
-		up: 15,
-		down: 0
-	},
-	"classes": {
-		"initial": "animated",
-		"pinned": "slideDown",
-		"unpinned": "slideUp"
-	}
-});
+document.addEventListener('DOMContentLoaded', function () { 
+	var headroom = new Headroom(document.querySelector(".site-header"), {
+		"offset": 150,
+		tolerance: {
+			up: 15,
+			down: 0
+		},
+		"classes": {
+			"initial": "animated",
+			"pinned": "slideDown",
+			"unpinned": "slideUp"
+		}
+	});
 
-headroom.init();
+	headroom.init();
+});
