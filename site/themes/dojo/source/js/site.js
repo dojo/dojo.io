aa811aa48d2a076549640ff0f9a2e87061c7f3ec
Nick Nisi <nick@nisi.org>
2017-03-22 20:51:25 -0500

    add toggle solution event listeners to site.js
    * Add a delegated event listener for toggle solution buttons, only if one is detected on the page
    * add 'hidden' class

--- site/themes/dojo/source/js/site.js
+++ site/themes/dojo/source/js/site.js
@@ -1,16 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () { 
-	var headroom = new Headroom(document.querySelector(".site-header"), {
-		"offset": 150,
+	var headroom = new Headroom(document.querySelector('.site-header'), {
+		offset: 150,
 		tolerance: {
 			up: 15,
 			down: 0
 		},
-		"classes": {
-			"initial": "animated",
-			"pinned": "slideDown",
-			"unpinned": "slideUp"
+		classes: {
+			initial: 'animated',
+			pinned: 'slideDown',
+			unpinned: 'slideUp'
 		}
 	});
 
 	headroom.init();
+
+	function addShowSolutionsListener() {
+		var matches = document.body.matches || document.body.webkitMatchesSelector || document.body.mozMatchesSelector
+			|| document.body.msMatchesSelector || document.body.webkitMatchesSelector || document.body.matchesSelector;
+		document.querySelector('.page-content').addEventListener('click', function (event) {
+			if (matches.call(event.target, 'button.toggle-solution')) {
+				event.stopPropagation();
+				var sectionId = event.target.getAttribute('data-target');
+				var element = document.getElementById(sectionId);
+				if (element) {
+					var isHidden = element.classList.contains('hidden');
+					element.classList[isHidden ? 'remove' : 'add']('hidden');
+				}
+			}
+		});
+	}
+
+	if (document.querySelector('button.toggle-solution')) {
+		addShowSolutionsListener();
+	}
 });
