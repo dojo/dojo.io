59384066ce950bafb2573be7a17f72a480685003
Torrey Rice <rice@sitepen.com>
2017-03-24 13:06:40 -0700

    More tutorial fixing
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- site/scripts/solution.js
+++ site/scripts/solution.js
@@ -8,6 +8,6 @@ hexo.extend.tag.register('solution', ([name], text) => {
 	text = text.replace(/s*/,"");
 	var content = Prism.highlight(text, Prism.languages[lang]);
 	
-	return `<button class="toggle-solution" data-target="${name}">Toggle Solution</button>
-		<section id="${name}" class="hidden"><pre class="language-${lang}"><code>${content}</code></pre></section>`;
+	return `<div class="solution-container"><button class="toggle-solution" data-target="${name}">Toggle Solution</button>
+		<section id="${name}" class="hidden"><pre class="language-${lang}"><code>${content}</code></pre></section></div>`;
 }, { ends: true });
