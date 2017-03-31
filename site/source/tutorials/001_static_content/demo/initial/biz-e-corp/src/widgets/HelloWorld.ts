18bd548aaebb70c0ff9f3b69a7b26d4ec2ff95bb
Andy Richardson <rishson@gmail.com>
2017-03-31 14:25:19 +0100

    Updated with beta1 generated code

--- site/source/tutorials/001_static_content/demo/initial/biz-e-corp/src/widgets/HelloWorld.ts
+++ site/source/tutorials/001_static_content/demo/initial/biz-e-corp/src/widgets/HelloWorld.ts
@@ -1,9 +1,30 @@
-import { WidgetBase } from '@dojo/widget-core/WidgetBase';
-import { WidgetProperties } from '@dojo/widget-core/interfaces';
 import { v } from '@dojo/widget-core/d';
+import { DNode, WidgetProperties } from '@dojo/widget-core/interfaces';
+import { ThemeableMixin, theme } from '@dojo/widget-core/mixins/Themeable';
+import { WidgetBase } from '@dojo/widget-core/WidgetBase';
 
-export default class HelloWorld extends WidgetBase<WidgetProperties> {
-	render() {
-		return v('div', [ 'Hello, Dojo World!' ]);
+import * as css from './styles/HelloWorld.css';
+
+export interface HelloWorldProperties extends WidgetProperties {
+	stranger: boolean;
+	toggleStranger: Function;
+}
+
+export const HelloWorldBase = ThemeableMixin(WidgetBase);
+
+@theme(css)
+export default class HelloWorld extends HelloWorldBase<HelloWorldProperties> {
+
+	private onClick(): void {
+		this.properties.toggleStranger && this.properties.toggleStranger();
+	}
+
+	protected render(): DNode {
+		const classes = this.classes(
+			css.hello,
+			this.properties.stranger ? css.upsidedown : null
+		);
+
+		return v('div', { classes, onclick: this.onClick }, [ 'Hello, Dojo World!' ]);
 	}
 }
