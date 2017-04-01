1f9638db8ef025597d31cd772e58f586c8a02628
Torrey Rice <rice@sitepen.com>
2017-04-01 15:48:07 -0700

    WIP on codefile tag plugin
    Works as long as you put in all the params in the tag… need to check for empty and handle better.
    
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- site/scripts/codefile.js
+++ site/scripts/codefile.js
@@ -7,48 +7,68 @@ var pathFn = require('path');
 var Prism = require('prismjs');
 var _ = require("lodash");
 
-hexo.extend.tag.register('codefile', function(args) {
-    var content;
+var cf = {
+    parseLinesRange: function(str) {
+        
+        var lines = (typeof str !== 'undefined') ? str : false;
+        
+        if (str.indexOf("-") != -1) {
+            lines = str.split("-");
+            lines[0] = (lines[0]) - 1;
+        } else {
+            lines = (str) -1;
+        }
+        
+        return lines;
+    },
+
+    getFilePath: function(path, filename) {
+        var dir = path.split('/').slice(0, -1).join('/');
+        return pathFn.join(hexo.source_dir, dir, filename);
+    }
+}
+
+hexo.extend.tag.register('include_codefile', function(args) {
+    var filename = args[0];
     var lang;
     var lines;
 
-    // Create object from passed in args
-    var tagArgs = {};
-    _.forEach(args, function(arg) {
-        var split = arg.split(":");
-        _.set(tagArgs, split[0], split[1]);
+    _.forEach(args.slice(1), function(arg){
+        if(_.startsWith(arg, "lang:")) {
+            lang = arg.replace("lang:", "");
+        }
+        if(_.startsWith(arg, "lines:")){ 
+            lines = arg.replace("lines:", "");
+        }
     });
 
-    // Create file path string
-    var dir = (this.path.split('/'));
-    dir = dir.slice(0, dir.length - 1).toString().replace(/,/g, "/");
-    var path = pathFn.join(hexo.source_dir, dir, tagArgs.file);
+    lines = cf.parseLinesRange(lines);
+    lang = (typeof lang !== 'undefined') ? lang : 'javascript';
 
-    // Check if language was passed in
-    lang = tagArgs.lang ? tagArgs.lang : "javascript"
-
-    // Check if file exists
-    if (fs.existsSync(path)) {
-        raw = fs.readFileSync(path);
+    var content = '';
+    var rawContent = fs.readFileSync(cf.getFilePath(this.path, filename));
+    
+    
+    if(lines) {
+        var linefeed = rawContent.indexOf('\r\n') !== -1 ? '\r\n' : '\n';
+        var splitContent = rawContent.split(linefeed)
+        
+        if(_.isArray(lines)) {
+            var lineStart = lines[0];
+            var lineEnd = lines[1];
+            var arrContent = splitContent.slice(lineStart, lineEnd);
+            _.each(arrContent, function (ln) {
+                content += ln + linefeed;
+            });
+        } else {
+            var content = splitContent[lines];
+        }
     } else {
-        return;
+        content = rawContent;
     }
 
-    // Check if range passed
-    if (tagArgs.lines) {
-        var lineStart = tagArgs.lines.split("-")[0];
-        var lineEnd = tagArgs.lines.split("-")[1];
-        var split = raw.split("\n").slice(lineStart, lineEnd);
-
-        _.each(split, function(v) {
-            content += v + "\n";
-        });
-        console.log(content);
-    } else {
-        content = raw;
-    }
-
-    var highlighted = Prism.highlight(content, Prism.languages[lang]);
-    return `<pre class="language-${lang}"><code>${highlighted}</code></pre>`;
+   var highlighted = Prism.highlight(content, Prism.languages[lang]);
+   return `<pre class="language-${lang}"><code>${highlighted}</code></pre>`;
 
 }, { async: true });
+
