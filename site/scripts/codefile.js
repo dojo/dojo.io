b9bcec0758720f3c7801303b1f1e7c1c57508558
Torrey Rice <rice@sitepen.com>
2017-04-02 10:16:41 -0700

    Fixing codefile include to support line or lines ranges
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- site/scripts/codefile.js
+++ site/scripts/codefile.js
@@ -2,67 +2,38 @@
 // Imports a code file and highlights the result.
 // File path must be a child .md file.
 
+/*
+    Usage: {% include_codefile path/to/file/relative/filename.ts [lines:1-4]|[line:4] [lang:ts] %}
+*/
+
 var fs = require('hexo-fs');
 var pathFn = require('path');
 var Prism = require('prismjs');
 var _ = require("lodash");
 
-var cf = {
-    parseLinesRange: function(str) {
-        
-        var lines = (typeof str !== 'undefined') ? str : false;
-        
-        if (str.indexOf("-") != -1) {
-            lines = str.split("-");
-            lines[0] = (lines[0]) - 1;
-        } else {
-            lines = (str) -1;
-        }
-        
-        return lines;
-    },
-
-    getFilePath: function(path, filename) {
-        var dir = path.split('/').slice(0, -1).join('/');
-        return pathFn.join(hexo.source_dir, dir, filename);
-    }
-}
 
 hexo.extend.tag.register('include_codefile', function(args) {
     var filename = args[0];
-    var lang;
-    var lines;
+    var lang = false;
+    var lines = false;
+    var content = '';
 
     _.forEach(args.slice(1), function(arg){
         if(_.startsWith(arg, "lang:")) {
             lang = arg.replace("lang:", "");
         }
-        if(_.startsWith(arg, "lines:")){ 
-            lines = arg.replace("lines:", "");
+        if (_.startsWith(arg, "line")) {
+            lines = arg.replace(/([a-zA-Z]+?)(s\b|\b):/gm, "");
+            lines = cf.parseLinesRange(lines);
         }
     });
 
-    lines = cf.parseLinesRange(lines);
-    lang = (typeof lang !== 'undefined') ? lang : 'javascript';
+    lang = lang ? lang : 'javascript';
 
-    var content = '';
     var rawContent = fs.readFileSync(cf.getFilePath(this.path, filename));
     
-    
-    if(lines) {
-        var linefeed = rawContent.indexOf('\r\n') !== -1 ? '\r\n' : '\n';
-        var splitContent = rawContent.split(linefeed)
-        
-        if(_.isArray(lines)) {
-            var lineStart = lines[0];
-            var lineEnd = lines[1];
-            var arrContent = splitContent.slice(lineStart, lineEnd);
-            _.each(arrContent, function (ln) {
-                content += ln + linefeed;
-            });
-        } else {
-            var content = splitContent[lines];
-        }
+    if(lines !== false) {
+       content = cf.extractContent(rawContent, lines);
     } else {
         content = rawContent;
     }
@@ -72,3 +43,41 @@ hexo.extend.tag.register('include_codefile', function(args) {
 
 }, { async: true });
 
+
+var cf = {
+    parseLinesRange: function (str) {
+        
+        if (str.indexOf("-") != -1) {
+            lines = str.split("-");
+            lines[0] = (lines[0]) - 1;
+        } else {
+            lines = (str) - 1;
+        }
+
+        return lines;
+    },
+
+    getFilePath: function (path, filename) {
+        var dir = path.split('/').slice(0, -1).join('/');
+        return pathFn.join(hexo.source_dir, dir, filename);
+    },
+
+    extractContent: function (rawContent, lines) {
+        var linefeed = rawContent.indexOf('\r\n') !== -1 ? '\r\n' : '\n';
+        var splitContent = rawContent.split(linefeed)
+        var fragment = "";
+
+        if (_.isArray(lines)) {
+            var lineStart = lines[0];
+            var lineEnd = lines[1];
+            var arrContent = splitContent.slice(lineStart, lineEnd);
+            _.each(arrContent, function (ln) {
+                fragment += ln + linefeed;
+            });
+        } else {
+            fragment = splitContent[lines];
+        }
+
+        return fragment;
+    }
+}
