730faaf60caac5b40a699813b66697924412002a
smhigley <shigley@sitepen.com>
2017-04-12 17:05:15 -0700

    move form state to App

--- site/source/tutorials/005_form_widgets/demo/finished/biz-e-corp/src/widgets/WorkerForm.ts
+++ site/source/tutorials/005_form_widgets/demo/finished/biz-e-corp/src/widgets/WorkerForm.ts
@@ -1,7 +1,6 @@
 import { WidgetBase } from '@dojo/widget-core/WidgetBase';
 import { WidgetProperties } from '@dojo/widget-core/interfaces';
 import { v, w } from '@dojo/widget-core/d';
-import { StatefulMixin } from '@dojo/widget-core/mixins/Stateful';
 import { ThemeableMixin, ThemeableProperties, theme } from '@dojo/widget-core/mixins/Themeable';
 import Button, { ButtonProperties } from '@dojo/widgets/button/Button';
 import TextInput, { TextInputProperties } from '@dojo/widgets/textinput/TextInput';
@@ -9,54 +8,39 @@ import { WorkerProperties } from './Worker';
 import * as styles from '../styles/workerForm.css';
 
 export interface WorkerFormProperties extends ThemeableProperties {
-	validateField?(field: string, value: string): boolean;
-	onSubmit?(worker: WorkerProperties): void;
+	firstName: string;
+	firstNameInvalid: boolean;
+	lastName: string;
+	lastNameInvalid: boolean;
+	email: string;
+	emailInvalid: boolean;
+	onChange?(field: string, value: string): void;
+	onBlur?(field: string, value: string): void;
+	onSubmit?(event: Event): void;
 }
 
-export const WorkerFormBase = StatefulMixin(ThemeableMixin(WidgetBase));
+export const WorkerFormBase = ThemeableMixin(WidgetBase);
 
 @theme(styles)
 export default class WorkerForm extends WorkerFormBase<WorkerFormProperties> {
-	private _clearValues() {
-		for (let field in this.state) {
-			this.setState({ [field]: undefined });
-		}
-	}
 
 	private _onSubmit(event: Event) {
 		event.preventDefault();
 
-		const {
-			onSubmit,
-			validateField = () => true
-		} = this.properties;
-
-		const {
-			firstName,
-			lastName,
-			email
-		} = this.state;
-
-		let validated = true;
-		for (const field of ['firstName', 'lastName', 'email']) {
-			if (!validateField(field, this.state[field])) {
-				validated = false;
-				break;
-			}
-		}
-
-		if (validated) {
-			onSubmit && onSubmit({ firstName, lastName, email });
-			this._clearValues();
-		}
-		else {
-			console.warn('There were errors in the form. Please check all fields and try again');
-		}
+		const { onSubmit } = this.properties;
+		onSubmit && onSubmit(event);
 	}
 
 	render() {
 		const {
-			validateField = () => true
+			firstName,
+			firstNameInvalid,
+			lastName,
+			lastNameInvalid,
+			email,
+			emailInvalid,
+			onChange,
+			onBlur
 		} = this.properties;
 
 		return v('form', {
@@ -72,13 +56,16 @@ export default class WorkerForm extends WorkerFormBase<WorkerFormProperties> {
 						content: 'First Name',
 						hidden: true
 					},
-					invalid: <boolean> this.state.firstNameInvalid,
+					invalid: <boolean> firstNameInvalid,
 					placeholder: 'Given name',
-					value: <string> this.state.firstName,
-					onChange: (event: Event) => this.setState({ 'firstName': (<HTMLInputElement> event.target).value }),
+					value: <string> firstName,
+					onChange: (event: Event) => {
+						const value = (<HTMLInputElement> event.target).value;
+						onChange && onChange('firstName', value);
+					},
 					onBlur: (event: Event) => {
 						const value = (<HTMLInputElement> event.target).value;
-						this.setState({ 'firstNameInvalid': !validateField('firstName', value) });
+						onBlur && onBlur('firstName', value);
 					}
 				}),
 				w<TextInputProperties>(TextInput, {
@@ -88,13 +75,16 @@ export default class WorkerForm extends WorkerFormBase<WorkerFormProperties> {
 						content: 'Last Name',
 						hidden: true
 					},
-					invalid: <boolean> this.state.lastNameInvalid,
+					invalid: <boolean> lastNameInvalid,
 					placeholder: 'Family name',
-					value: <string> this.state.lastName,
-					onChange: (event: Event) => this.setState({ 'lastName': (<HTMLInputElement> event.target).value }),
+					value: <string> lastName,
+					onChange: (event: Event) => {
+						const value = (<HTMLInputElement> event.target).value;
+						onChange && onChange('lastName', value);
+					},
 					onBlur: (event: Event) => {
 						const value = (<HTMLInputElement> event.target).value;
-						this.setState({ 'lastNameInvalid': !validateField('lastName', value) });
+						onBlur && onBlur('lastName', value);
 					}
 				}),
 			]),
@@ -102,12 +92,15 @@ export default class WorkerForm extends WorkerFormBase<WorkerFormProperties> {
 				key: 'input3',
 				type: 'email',
 				label: 'Email address',
-				invalid: <boolean> this.state.emailInvalid,
-				value: <string> this.state.email,
-				onChange: (event: Event) => this.setState({ 'email': (<HTMLInputElement> event.target).value }),
+				invalid: <boolean> emailInvalid,
+				value: <string> email,
+				onChange: (event: Event) => {
+					const value = (<HTMLInputElement> event.target).value;
+					onChange && onChange('email', value);
+				},
 				onBlur: (event: Event) => {
 					const value = (<HTMLInputElement> event.target).value;
-					this.setState({ 'emailInvalid': !validateField('email', value) });
+					onBlur && onBlur('email', value);
 				}
 			}),
 			w<ButtonProperties>(Button, {
