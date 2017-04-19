14fe6bc6ce2bd0a9b83014579efb131400e9cff0
Paul <a@devpaul.com>
2017-04-19 10:51:06 -0700

    fixed api index build (#85)

--- support/commands/hexo.ts
+++ support/commands/hexo.ts
@@ -1,43 +1,25 @@
 import { join } from 'path';
 import { exec, promisify } from '../util/process';
-import { readdirSync } from 'fs';
-import { lstatSync } from 'fs';
-import * as semver from 'semver';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { EOL } from 'os';
-import { writeFileSync } from 'fs';
+import getApiPackages, { DocumentedApi } from './getApiPackages';
 
 function generateHexo(siteDirectory: string, configs: string[] = [ '_config.yml' ]) {
 	const hexoBin = join('node_modules', '.bin', 'hexo');
 	const proc = exec(`${ hexoBin } --config=${ configs.join(',') } generate`, { silent: false, cwd: siteDirectory });
+	console.log(`${ hexoBin } --config=${ configs.join(',') } generate`);
 	return promisify(proc);
 }
 
-interface DocumentedApi {
-	latest: string;
-	package: string;
-	versions: string[];
-}
+export function buildApiIndex(apis: DocumentedApi[], source: string): string {
+	const template = readFileSync(source);
+	const links: string[] = [];
 
-function buildApiIndex(apiDirectory: string, source: string, target: string) {
-	const apis: DocumentedApi[] = readdirSync(apiDirectory).filter(function (file: string) {
-		const stat = lstatSync(join(apiDirectory, file));
-		return stat.isDirectory() && file.charAt(0) !== '_';
-	}).map(function (dir: string) {
-		const versions = readdirSync(join(apiDirectory, dir)).sort(semver.compare);
-
-		return {
-			latest: versions[0],
-			package: dir,
-			versions
-		};
-	});
-
-	let apiTemplate = readFileSync(source).toString('utf-8');
 	for (let api of apis) {
-		apiTemplate += `[${ api.package }](/api/${ api.package }/${ api.latest })${ EOL }`;
+		links.push(`[${ api.package }](/api/${ api.package }/${ api.latest })`);
 	}
-	writeFileSync(target, apiTemplate);
+
+	return template + links.join(EOL);
 }
 
 export interface Options {
@@ -50,6 +32,8 @@ export interface Options {
 
 export default async function hexo(options: Options) {
 	const { apiDirectory, apiTemplate, apiTarget, siteDirectory, configs } = options;
-	buildApiIndex(apiDirectory, apiTemplate, apiTarget);
+	const apis = getApiPackages(apiDirectory);
+
+	writeFileSync(apiTarget, buildApiIndex(apis, apiTemplate));
 	await generateHexo(siteDirectory, configs);
 }
