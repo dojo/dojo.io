45a407ffb8d071f550068c84caaa0144690ee88e
Anthony Gubler <agubler@sitepen.com>
2017-05-08 14:50:48 +0100

    Upgrade for latest widget-core

--- site/source/tutorials/005_form_widgets/demo/finished/biz-e-corp/src/widgets/Banner.ts
+++ site/source/tutorials/005_form_widgets/demo/finished/biz-e-corp/src/widgets/Banner.ts
@@ -1,9 +1,9 @@
 import { WidgetBase } from '@dojo/widget-core/WidgetBase';
-import { WidgetProperties } from '@dojo/widget-core/interfaces';
+import { DNode, WidgetProperties } from '@dojo/widget-core/interfaces';
 import { v } from '@dojo/widget-core/d';
 
 export default class Banner extends WidgetBase<WidgetProperties> {
-	render(): DNode {
+	protected render(): DNode {
 		return v('h1', { title: 'I am a title!' }, [ 'Biz-E Bodies' ]);
 	}
 }
