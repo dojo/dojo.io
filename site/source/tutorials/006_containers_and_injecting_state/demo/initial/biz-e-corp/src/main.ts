35883d2278f343f527ea6e5fa58afa070190efb7
Michael Van Sickle <mvansickle1@gmail.com>
2017-06-16 09:14:01 -0400

    remove old state container tutorial directory

--- site/source/tutorials/006_containers_and_injecting_state/demo/initial/biz-e-corp/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { ProjectorMixin } from '@dojo/widget-core/mixins/Projector';
-import App from './widgets/App';
-
-const root = document.querySelector('my-app') || undefined;
-
-const Projector = ProjectorMixin(App);
-const projector = new Projector();
-
-projector.append(root);
