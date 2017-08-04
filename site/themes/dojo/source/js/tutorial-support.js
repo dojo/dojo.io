2f834ebfbb12f7fffea829d38271f0425f1a190a
Travis <bmaier@sitepen.com>
2017-08-03 22:28:04 -0400

    Widen tutorial content and improve navigation

--- site/themes/dojo/source/js/tutorial-support.js
+++ site/themes/dojo/source/js/tutorial-support.js
@@ -20,9 +20,18 @@ tutorial.populateActionBar = function(container) {
 	ownEditorButton.innerHTML = 'Begin tutorial';
 	sectionOneContainer.appendChild(ownEditorButton);
 
+	var backButton = document.createElement('button');
+	backButton.classList.add('previous', 'green');
+	backButton.innerHTML = 'Back';
+	otherSectionContainer.appendChild(backButton);
+
 	var sections = document.querySelectorAll('section.tutorial');
 	var sectionContainer = document.createElement('div');
+	var centerSectionContainer = document.createElement('div');
+	centerSectionContainer.className = 'center-section-container';
 	sectionContainer.className = 'section-container';
+
+	sectionContainer.appendChild(centerSectionContainer);
 	otherSectionContainer.appendChild(sectionContainer);
 
 	var nextButton = document.createElement('button');
@@ -35,11 +44,11 @@ tutorial.populateActionBar = function(container) {
 	// create each section selector and link it to its section via data-section-num
 	if (sections.length > 0) {
 		for (var i = 0; i < sections.length; i++) {
+			sections[i].setAttribute('data-section-num', i);
 			if (i === 0) {
 				sections[i].classList.remove('hidden', 'hiding');
 				continue;
 			}
-			sections[i].setAttribute('data-section-num', i);
 			var sectionSelector = document.createElement('div');
 			sectionSelector.setAttribute('data-section-num', i);
 			sectionSelector.className = 'section-selector';
@@ -48,7 +57,7 @@ tutorial.populateActionBar = function(container) {
 				sectionSelector.title = title.textContent;
 			}
 
-			sectionContainer.appendChild(sectionSelector);
+			centerSectionContainer.appendChild(sectionSelector);
 			sectionSelectors.push(sectionSelector);
 		}
 	}
@@ -59,7 +68,7 @@ tutorial.populateActionBar = function(container) {
 		activateSelector(document.querySelector('.section-selector[data-section-num="1"]'));
 	});
 
-	sectionContainer.addEventListener('click', function (e) {
+	centerSectionContainer.addEventListener('click', function (e) {
 		var target = e.target;
 		// short-circuit if clicked section is already active
 		if (target.classList.contains('active')) {
@@ -68,26 +77,15 @@ tutorial.populateActionBar = function(container) {
 		activateSelector(target);
 	});
 
-	nextButton.addEventListener('click', function () {
-		var activeSelector = document.querySelector('.section-selector.active');
-		var sectionNumber = 0;
-		if (activeSelector) {
-			sectionNumber = parseInt(activeSelector.getAttribute('data-section-num'), 10) + 1;
-		} else {
-			sectionNumber = 1;
-		}
-		var newSelector = document.querySelector('.section-selector[data-section-num="' + sectionNumber + '"]');
-		if (!newSelector) {
-			return;
-		}
-		activateSelector(newSelector);
-
-
-
+	backButton.addEventListener('click', function () {
+		navigate(-1);
 	});
 
+	nextButton.addEventListener('click', function () {
+		navigate(1);
+	});
 	function activateSelector(target) {
-		var sectionToActivate = target.getAttribute('data-section-num');
+		var sectionToActivate = (target.getAttribute && target.getAttribute('data-section-num')) || target;
 
 		sectionSelectors.forEach(function (sectionSelector) {
 			if (sectionSelector === target) {
@@ -99,7 +97,8 @@ tutorial.populateActionBar = function(container) {
 
 		for (var i = 0; i < sections.length; i++) {
 			var section = sections[i];
-			if (section.getAttribute('data-section-num') === sectionToActivate) {
+			const sectionNum = section.getAttribute('data-section-num');
+			if (sectionNum === sectionToActivate) {
 				section.classList.remove('hidden');
 			} else {
 				section.classList.add('hiding');
@@ -117,12 +116,28 @@ tutorial.populateActionBar = function(container) {
 			}
 			document.querySelector('.scrollable').scrollTop = 0;
 
-			if (parseInt(sectionToActivate, 10) === sections.length - 1) {
+			var sectionToActivateNum = sectionToActivate && parseInt(sectionToActivate, 10);
+			if (sectionToActivateNum === sections.length - 1) {
 				nextButton.classList.add('hidden');
 			} else {
 				nextButton.classList.remove('hidden');
 			}
+			if (!sectionToActivateNum) {
+				sectionOneContainer.classList.remove('hidden');
+				otherSectionContainer.classList.add('hidden');
+			}
 		}, 300, sectionToActivate);
 	}
 
-}
+	function navigate(moveBy) {
+		var activeSelector = document.querySelector('.section-selector.active');
+		var sectionNumber = 0;
+		if (activeSelector) {
+			sectionNumber = parseInt(activeSelector.getAttribute('data-section-num'), 10) + moveBy;
+		} else {
+			sectionNumber = 1;
+		}
+		var newSelector = document.querySelector('.section-selector[data-section-num="' + sectionNumber + '"]');
+		activateSelector(newSelector || '0');
+	}
+};
