d1fe26c8e06ba4220b8899c5ad7944c58a791110
Bradley Maier <bmaier@sitepen.com>
2017-09-01 14:37:42 -0400

    Redirect paths by adding a trailing slash to workaround webserv issue

--- support/grunt/config/webserv.ts
+++ support/grunt/config/webserv.ts
@@ -1,3 +1,5 @@
+import { statSync } from 'fs';
+import { join } from 'path';
 import ServeFile from 'webserv/middleware/ServeFile';
 import ServeDirectory from 'webserv/middleware/ServeDirectory';
 import WebProxy from 'webserv/middleware/Proxy';
@@ -6,12 +8,36 @@ import { relativeUrl } from 'webserv/handlers/transform';
 import LogRequest from 'webserv/middleware/LogRequest';
 import { Handler } from 'webserv/handlers/Handler';
 
+function isDirectory(location: string): boolean {
+	try {
+		const stats = statSync(location);
+		return stats.isDirectory();
+	}
+	catch (e) {
+	}
+
+	return false;
+}
+
+const rootDirectory = './_dist';
+
 export function middleware(): Handler[] {
 	return [
 		new LogRequest(),
+		route(/./).filter(incomingMessage => {
+			const path = join(rootDirectory, incomingMessage.url);
+
+			return isDirectory(path) && incomingMessage.url.lastIndexOf('/') !== incomingMessage.url.length - 1;
+		}).wrap(
+			(request, response) => {
+				response.writeHead(301, { Location: request.url + '/'});
+				response.end('');
+				return Promise.resolve();
+			}
+		),
 		route(/./).transform(relativeUrl('/dojo.io')).wrap([
-			new ServeFile('./_dist'),
-			new ServeDirectory('./_dist')
+			new ServeFile(rootDirectory),
+			new ServeDirectory(rootDirectory)
 		]),
 		new WebProxy('http://dojo.github.io')
 	];
