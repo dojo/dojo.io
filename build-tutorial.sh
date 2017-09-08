88567ac32a3c78240788eaf1f570dd21c0b5ecc3
Bradley Maier <bmaier@sitepen.com>
2017-09-08 11:47:47 -0400

    Add build script

---
+++ build-tutorial.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ev
+pushd .
+if [ -d "site/source/tutorials/$TUTORIAL/demo/initial/biz-e-corp" ]; then
+    cd site/source/tutorials/$TUTORIAL/demo/initial/biz-e-corp
+    npm install
+    dojo build
+fi
+popd
+pushd .
+if [ -d "site/source/tutorials/$TUTORIAL/demo/finished/biz-e-corp ]; then
+    cd site/source/tutorials/$TUTORIAL/demo/finished/biz-e-corp
+    npm install
+    dojo test
+fi
+popd
+
+exit 0;
