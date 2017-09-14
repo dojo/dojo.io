0a454e8b94a0395e5daf5925a716ff321d985ce7
Travis CI User <travis@example.org>
2017-09-14 19:45:03 +0000

    Published by Travis CI User from commit 64cacd71b4f7ab32c2165ba58b919b654b1fe67a

---
+++ tutorials/1020_registries/demo/finished/biz-e-corp/src/styles/worker.css.d.ts
@@ -0,0 +1,5 @@
+export const image: string;
+export const worker: string;
+export const workerFront: string;
+export const workerBack: string;
+export const reverse: string;
---
+++ tutorials/1020_registries/demo/finished/biz-e-corp/src/widgets/Worker.ts
@@ -0,0 +1,71 @@
+import { WidgetBase } from '@dojo/widget-core/WidgetBase';
+import { v, w } from '@dojo/widget-core/d';
+import { theme, ThemeableMixin } from '@dojo/widget-core/mixins/Themeable';
+
+import WorkerBack from './WorkerBack';
+import * as css from '../styles/worker.css';
+
+export interface WorkerProperties {
+	firstName?: string;
+	lastName?: string;
+	email?: string;
+	timePerTask?: number;
+	tasks?: string[];
+}
+
+const WorkerBase = ThemeableMixin(WidgetBase);
+
+@theme(css)
+export default class Worker extends WorkerBase<WorkerProperties> {
+	private _isFlipped = false;
+
+	protected render() {
+		return v('div', {
+			classes: this.classes(css.worker, this._isFlipped ? css.reverse : null)
+		}, [
+			this._renderFront(),
+			this._renderBack()
+		]);
+	}
+
+	private _renderFront() {
+		const {
+			firstName = 'firstName',
+			lastName = 'lastName'
+		} = this.properties;
+
+		return v('div', {
+				classes: this.classes(css.workerFront),
+				onclick: this.flip
+			}, [
+				v('img', {
+					classes: this.classes(css.image),
+						src: 'images/worker.svg' }, []),
+				v('div', [
+					v('strong', [ `${lastName}, ${firstName}` ])
+				])
+			]
+		);
+	}
+
+	private _renderBack() {
+		const {
+			firstName = 'firstName',
+			lastName = 'lastName',
+			email = 'unavailable',
+			timePerTask = 0,
+			tasks = []
+		} = this.properties;
+
+		return v('div', {
+				classes: this.classes(css.workerBack),
+				onclick: this.flip
+			}, [ this._isFlipped ? w<WorkerBack>('worker-back', { firstName, lastName, email, timePerTask, tasks }) : null ]
+		);
+	}
+
+	flip(): void {
+		this._isFlipped = !this._isFlipped;
+		this.invalidate();
+	}
+}
