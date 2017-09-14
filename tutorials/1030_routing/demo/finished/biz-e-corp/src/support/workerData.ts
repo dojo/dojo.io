df063e33d7aa5b0c82e88e961d1c23254e9e1c26
Travis CI User <travis@example.org>
2017-09-14 19:04:35 +0000

    Published by Travis CI User from commit 3242d9879bb726a0b12a300dde4c59e715943cc3

---
+++ tutorials/1030_routing/demo/finished/biz-e-corp/src/support/workerData.ts
@@ -0,0 +1,22 @@
+export const workerData = [
+	{
+		firstName: 'Tim',
+		lastName: 'Jones',
+		email: 'tim.jones@bizecorp.org',
+		tasks: [
+			'6267 - Untangle paperclips',
+			'4384 - Shred documents',
+			'9663 - Digitize 1985 archive'
+		]
+	},
+	{
+		firstName: 'Alicia',
+		lastName: 'Fitzgerald'
+	},
+	{
+		firstName: 'Hans',
+		lastName: 'Mueller'
+	}
+];
+
+export default workerData;
---
+++ tutorials/1030_routing/demo/finished/biz-e-corp/src/widgets/WorkerContainer.ts
@@ -0,0 +1,55 @@
+import { WidgetBase } from '@dojo/widget-core/WidgetBase';
+import { w, v } from '@dojo/widget-core/d';
+import Worker, { WorkerProperties } from './Worker';
+import { theme, ThemeableMixin } from '@dojo/widget-core/mixins/Themeable';
+import * as css from '../styles/workerContainer.css';
+
+import { Link } from '@dojo/routing/Link';
+
+export interface WorkerContainerProperties {
+	workerData: WorkerProperties[];
+	filter?: string;
+}
+
+const WorkerContainerBase = ThemeableMixin(WidgetBase);
+
+@theme(css)
+export default class WorkerContainer extends WorkerContainerBase<WorkerContainerProperties> {
+
+	private _createFilterLinks() {
+		const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		const links = [];
+		for (let i = 0; i < alphabet.length; i++) {
+			const char = alphabet.charAt(i);
+			links.push(
+				v('span', { classes: this.classes(css.links) }, [
+					w(Link, { key: char, to: 'filter', params: { filter: char }}, [ char ])
+				])
+			);
+		}
+		return links;
+	}
+
+	protected render() {
+		const {
+			workerData = [],
+			filter
+		} = this.properties;
+
+		const workers = workerData.filter((worker) => {
+			if (filter) {
+				return worker.lastName && worker.lastName.charAt(0).toUpperCase() === filter;
+			}
+			return true;
+		}).map((worker, i) => w(Worker, {
+			key: `worker-${i}`,
+			...worker
+		}));
+
+		return v('div', {}, [
+			v('h1', { classes: this.classes(css.title) }, [ 'Worker Directory' ]),
+			v('div', { classes: this.classes(css.filters) }, this._createFilterLinks()),
+			v('div', { classes: this.classes(css.container) }, workers)
+		]);
+	}
+}
