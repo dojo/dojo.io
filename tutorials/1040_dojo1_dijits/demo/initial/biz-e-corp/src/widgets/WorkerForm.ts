3e2c288d4c954a78adbf3983c6a088da0189595c
Travis CI User <travis@example.org>
2017-09-29 11:04:56 +0000

    Published by Travis CI User from commit 65a61554a2a1298fcded1adf195071d2fb2cd27f

---
+++ tutorials/1040_dojo1_dijits/demo/initial/biz-e-corp/src/widgets/WorkerForm.ts
@@ -0,0 +1,47 @@
+import { WidgetBase } from '@dojo/widget-core/WidgetBase';
+import { TypedTargetEvent } from '@dojo/widget-core/interfaces';
+import { v } from '@dojo/widget-core/d';
+import { ThemeableMixin, theme } from '@dojo/widget-core/mixins/Themeable';
+import * as css from '../styles/workerForm.css';
+
+export interface WorkerFormData {
+	firstName: string;
+	lastName: string;
+	email: string;
+}
+
+export interface WorkerFormProperties {
+	formData: Partial<WorkerFormData>;
+	onFormInput: (data: Partial<WorkerFormData>) => void;
+	onFormSave: () => void;
+}
+
+export const WorkerFormBase = ThemeableMixin(WidgetBase);
+
+@theme(css)
+export default class WorkerForm extends WorkerFormBase<WorkerFormProperties> {
+
+	private _onSubmit(event: Event) {
+		event.preventDefault();
+		this.properties.onFormSave();
+	}
+
+	protected onFirstNameInput({ target: { value: firstName } }: TypedTargetEvent<HTMLInputElement> & DocumentEvent) {
+		this.properties.onFormInput({ firstName });
+	}
+
+	protected onLastNameInput({ target: { value: lastName } }: TypedTargetEvent<HTMLInputElement> & DocumentEvent) {
+		this.properties.onFormInput({ lastName });
+	}
+
+	protected onEmailInput({ target: { value: email } }: TypedTargetEvent<HTMLInputElement> & DocumentEvent) {
+		this.properties.onFormInput({ email });
+	}
+
+	protected render() {
+		return v('form', {
+			classes: this.classes(css.workerForm),
+			onsubmit: this._onSubmit
+		}, [ ]);
+	}
+}
