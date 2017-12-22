fed0d8ede5545658cf9b9661872c9f0f4bd34052
Travis CI User <travis@example.org>
2017-12-22 23:01:14 +0000

    Published by Travis CI User from commit 84108c6811cd3ec80eb36aa9eb70cf563ecb3692

--- tutorials/007_theming/demo/finished/biz-e-corp/src/widgets/Banner.ts
+++ tutorials/007_theming/demo/finished/biz-e-corp/src/widgets/Banner.ts
@@ -3,10 +3,10 @@ import { v } from '@dojo/widget-core/d';
 import { theme, ThemedMixin } from '@dojo/widget-core/mixins/Themed';
 import * as css from '../styles/banner.m.css';
 
-const WorkerBase = ThemedMixin(WidgetBase);
+const BannerBase = ThemedMixin(WidgetBase);
 
 @theme(css)
-export default class Banner extends WorkerBase {
+export default class Banner extends BannerBase {
 	protected render() {
 		return v('h1', { title: 'I am a title!', classes: this.theme(css.root) }, [ 'Biz-E-Bodies' ]);
 	}
