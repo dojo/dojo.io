f45e61bf7fa897fe1040862115683d8008202cc1
Jason Cheatham <j.cheatham@gmail.com>
2018-04-04 23:11:24 -0400

    Generate table-of-contents for doc pages
    * Remove existing TOCs from READMEs, generate new one from document
      headings and add to a right sidebar
    * Use UIkit scrollspy to track doc position and highlight current
      section in TOC
    * Support jumping directly to proper section on page reload or during
      direct navigation
    * Update how sidebar padding is handled since we now have sidebars
      to the left and right of the content area (so a sibling selector
      is no longer sufficient)
    * Make doc headings clickable; clicking them updates the current
      location hash, similar to how github works.
    * Update the hash format to use CSS-selectable IDs; this allows doc and
      section IDs to be consistent everywhere (in headings, links, etc.)

--- site/themes/dojo/source/js/repo_docs.js
+++ site/themes/dojo/source/js/repo_docs.js
@@ -8,7 +8,7 @@
 	var markdown;
 
 	// Used to separate the document ID from an in-page anchor ID
-	var sep = '!';
+	var sep = '--';
 
 	// The list of available doc IDs
 	var docs = [];
@@ -19,6 +19,7 @@
 	// Get the doc container based on the location of this script in the DOM
 	var thisScript = document.currentScript;
 	var container = thisScript.parentElement;
+	var tocContainer = document.querySelector('.docs .sidebar-right');
 
 	// These are sources for github content. The CDNs will likely be faster than
 	// raw.githubusercontent, and are not (as) rate limited.
@@ -66,6 +67,39 @@
 	}
 
 	/**
+	 * Convert a doc ID (org/project/version) to a DOM-compatible ID
+	 * (org__project__version)
+	 */
+	function docIdToDomId(docId) {
+		return docId.replace(/\//g, '__');
+	}
+
+	/**
+	 * Convert a DOC ID (org__project__version) to a doc ID
+	 * (org/project/version)
+	 */
+	function domIdToDocId(domId) {
+		return domId.replace(/__/g, '/');
+	}
+
+	/**
+	 * Locate and remove a table-of-contents from a README. Return the TOC
+	 * element.
+	 */
+	function extractToc(elem) {
+		var child;
+
+		// Look through the first few elements for a likely candidate
+		for (var i = 0; i < 10; i++) {
+			child = elem.children[i];
+			if (child && isListOfLinks(child)) {
+				child.parentNode.removeChild(child);
+				return child;
+			}
+		}
+	}
+
+	/**
 	 * Return a doc ID corresponding to a location or href string. Currently, a
 	 * doc ID is just the hash part of a URL.
 	 */
@@ -87,6 +121,13 @@
 	}
 
 	/**
+	 * Return the current location has as a doc ID (org/project/version)
+	 */
+	function getHash() {
+		return domIdToDocId(location.hash.slice(1));
+	}
+
+	/**
 	 * Initialize the doc viewer
 	 */
 	function init() {
@@ -99,27 +140,38 @@
 		});
 
 		window.addEventListener('hashchange', function (event) {
-			var hash = location.hash.slice(1);
+			event.preventDefault();
+			var hash = getHash();
+			console.log('hash changed to ' + hash);
 
 			if (hash === currentDoc) {
 				// If the new hash is a base document ID, scroll back to the top
 				// of the doc
-				event.preventDefault();
 				resetScroll();
 			} else if (getDocId(hash) !== currentDoc) {
 				// If the new hash doesn't match the current document, render
 				// the new doc
-				event.preventDefault();
 				renderDoc(hash);
+			} else {
+				scrollTo('#' + docIdToDomId(hash));
 			}
 
 			// Otherwise just let the browser handle the event normally
 		});
 
+		// If the user clicks a heading, set the browser hash to the heading's
+		// ID and scroll to it
+		container.addEventListener('click', function (event) {
+			const target = event.target;
+			if (/H[2-4]/.test(target.tagName)) {
+				setHash(target.id);
+			}
+		});
 
 		// If the current path is a doc link, load the referenced doc
-		if (location.hash) {
-			renderDoc(location.hash);
+		var hash = getHash();
+		if (hash) {
+			renderDoc(hash);
 		}
 	}
 
@@ -169,11 +221,11 @@
 			var hrefParts = hrefToken[1].split('#');
 			var file = hrefParts[0];
 			var hash = hrefParts[1];
-			var docId = env.docId;
+			var domId = docIdToDomId(env.docId);
 
 			if (!file) {
 				// This is an in-page anchor link
-				hrefToken[1] = '#' + docId + sep + hash;
+				hrefToken[1] = '#' + domId + sep + hash;
 			} else if (/github.com/.test(file)) {
 				// This is a github link -- see if it's relative to our docs
 				var match = /https?:\/\/github.com\/(\w+)\/(\w+)/.exec(file);
@@ -188,7 +240,7 @@
 				}
 			} else if (!/^https?:/.test(file)) {
 				// This is a relative link
-				hrefToken[1] = '#' + docId + '/' + file;
+				hrefToken[1] = '#' + domId + '/' + file;
 			}
 
 			return defaultLinkRender(tokens, idx, options, env, self);
@@ -199,14 +251,18 @@
 			var token = tokens[idx];
 			var content = tokens[idx + 1].content;
 			var id = content;
-			var docId = env.docId;
-			var anchorId = docId + sep + slugify(id);
+			var domId = docIdToDomId(env.docId);
+			var anchorId = domId + sep + slugify(id);
 
 			// Create a separate <a> link above the heading tag. This allows us
 			// to position the anchor some distance above the heading, so that
 			// when the user clicks an anchor link the heading itself will be
 			// visible (not hidden under the top bar).
-			return '<a class="anchor" id="' + anchorId + '"></a><' + token.tag + '>';
+			// return '<a class="anchor" id="' + anchorId + '"></a><' + token.tag + '>';
+			var icon = token.tag.toLowerCase() !== 'h1' ?
+				'<span uk-icon="icon: link"></span>' :
+				'';
+			return '<' + token.tag + ' id="' + anchorId + '">' + icon;
 		};
 	}
 
@@ -233,15 +289,107 @@
 	}
 
 	/**
+	 * Return true if the given element appears to be a Table of Contents
+	 */
+	function isListOfLinks(elem) {
+		if (elem.tagName !== 'UL') {
+			return false;
+		}
+
+		var child = elem.firstElementChild;
+		var link;
+		var sublist;
+		while (child) {
+			if (child.tagName !== 'LI') {
+				return false;
+			}
+			link = child.firstElementChild;
+			if (!link || link.tagName !== 'A') {
+				return false;
+			}
+			sublist = link.nextElementSibling;
+			if (sublist && sublist.tagName !== 'UL') {
+				return false;
+			}
+			if (sublist && !isListOfLinks(sublist)) {
+				return false;
+			}
+			child = child.nextElementSibling;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Create a Table of Contents for a doc
+	 */
+	function makeToc() {
+		var headings = container.querySelectorAll('h2,h3,h4');
+
+		var toc = document.createElement('ul');
+		toc.classList.add('uk-nav');
+		toc.classList.add('uk-nav-default');
+		toc.setAttribute('uk-scrollspy-nav', 'closest: li; scroll: true; offset: 100');
+
+		var lastItem;
+		var level = 0;
+
+		headings.forEach(function (heading) {
+			var link = document.createElement('a');
+			link.href = '#' + heading.id;
+			link.textContent = heading.textContent;
+
+			var item = document.createElement('li');
+			item.appendChild(link);
+
+			// Get the level of the current heading
+			var headingLevel = Number(heading.tagName.slice(1)) - 2;
+
+			if (!lastItem) {
+				toc.appendChild(item);
+			} else if (headingLevel === level) {
+				lastItem.parentElement.appendChild(item);
+			} else if (headingLevel > level) {
+				var submenu = document.createElement('ul');
+				submenu.classList.add('uk-nav-sub');
+				submenu.classList.add('uk-nav-default');
+				submenu.appendChild(item);
+				lastItem.appendChild(submenu);
+			} else {
+				var parent = lastItem.parentElement;
+				var hlvl = level;
+				while (parent && hlvl > headingLevel) {
+					parent = parent.parentElement.parentElement;
+					hlvl--;
+				}
+				(parent || toc).appendChild(item);
+			}
+
+			lastItem = item;
+			level = headingLevel;
+		});
+
+		// UIkit will emit a 'scrolled' event when 
+		document.body.addEventListener('scrolled', function (event) {
+			var hash = event.target.hasAttribute('href') ?
+				event.target.getAttribute('href') :
+				'#' + event.target.id;
+			if (hash !== location.hash) {
+				history.pushState({}, '', hash);
+			}
+		});
+
+		return toc;
+	}
+
+
+	/**
 	 * Render a README for a particular repo and version.
 	 *
 	 * A doc ID has the format '<org>/<repo>/<version>[/<path>]'. It looks like
 	 * 'dojo/cli/master' or 'dojo/core/master/docs/math.md'.
 	 */
 	function renderDoc(hash) {
-		// Remove a leading #
-		hash = hash.replace(/^#/, '');
-
 		var parts = hash.split(sep);
 		var docId = parts[0];
 		var idParts = docId.split('/');
@@ -249,14 +397,19 @@
 		var version = idParts[2];
 		var path = idParts.slice(3).join('/') || 'README.md';
 
-		// Set the location hash to the base document ID. It will be set back to
-		// its original value once the document is rendered.
-		location.hash = '#' + parts[0];
-
 		return docFetch(repo + '/' + version + '/' + path)
 			.then(function (text) {
 				var content = markdown.render(text, { docId: docId });
 				container.innerHTML = content;
+
+				tocContainer.innerHTML = '';
+				extractToc(container);
+				var toc = makeToc();
+				tocContainer.appendChild(toc);
+
+				if (window.UIkit) {
+					window.UIkit.scrollspyNav(toc);
+				}
 
 				if (path !== 'README.md') {
 					// If we're not viewing the base README, add a link back to
@@ -271,12 +424,12 @@
 				}
 
 				currentDoc = docId;
-				resetScroll();
 
-				// Now that the page is loaded, reset the location hash to its
-				// original value so that the browser will scroll to any anchor
-				// indicated by the hash
-				location.hash = hash;
+				if (hash.indexOf(sep) === -1) {
+					resetScroll();
+				} else {
+					scrollTo('#' + docIdToDomId(hash));
+				}
 			});
 	}
 
@@ -285,6 +438,24 @@
 	 */
 	function resetScroll() {
 		document.body.parentElement.scrollTop = 0;
+	}
+
+	/**
+	 * Scroll to a given anchor
+	 */
+	function scrollTo(hash) {
+		window.UIkit.scroll(hash, { offset: 100 }).scrollTo(hash);
+	}
+
+	/**
+	 * Set the current location hash and scroll to the corresponding anchor
+	 */
+	function setHash(hash) {
+		if (hash[0] !== '#') {
+			hash = '#' + hash;
+		}
+		history.pushState({}, '', hash);
+		scrollTo(hash);
 	}
 
 	/**
