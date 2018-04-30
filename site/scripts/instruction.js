15ada740ace9a53374248e8105edaa926901b752
Torrey <rice@itorrey.com>
2018-04-30 12:12:37 -0700

    Removing old content and fixing api highlighting
    Signed-off-by: Torrey <rice@itorrey.com>

--- site/scripts/instruction.js
+++ site/scripts/instruction.js
@@ -1,25 +1,31 @@
-hexo.extend.tag.register('instruction', function(args) {
+hexo.extend.tag.register("instruction", function(args) {
 	return `<div class="instruction">${convertCodeAndLinks(args[0])}</div>`;
 });
 
 function convertCodeAndLinks(content) {
 	// replace < with &lt; and > with &gt;
-	content = content && content.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+	content = content && content.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 
 	// replace single asterisks with <em> tag
-	content = content && content.replace(/\*(.*?)\*/g, function (match, content) {
-		return `<em>${content}</em>`;
-	});
+	content =
+		content &&
+		content.replace(/\*(.*?)\*/g, function(match, content) {
+			return `<em>${content}</em>`;
+		});
 
 	// replace backticks with <code> tags
-	content = content && content.replace(/`(.*?)`/g, function (match, param) {
-		return `<code>${param}</code>`;
-	});
+	content =
+		content &&
+		content.replace(/`(.*?)`/g, function(match, param) {
+			return `<code>${param}</code>`;
+		});
 
 	// replace markdown for hyperlink with proper HTML
-	content = content && content.replace(/\[(.*?)\]\((.*?)\)/g, function (match, title, url) {
-		return `<a href='${url}'>${title}</a>`;
-	});
+	content =
+		content &&
+		content.replace(/\[(.*?)\]\((.*?)\)/g, function(match, title, url) {
+			return `<a href='${url}'>${title}</a>`;
+		});
 
 	return content;
 }
